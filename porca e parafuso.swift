33b442e91e46c40840ecf0a35b90ce9bc8af3c42
kaylanrfr <60267658+kaylanrfr@users.noreply.github.com>
2020-11-22 21:22:55 -0300

    Update porca e parafuso.swift

--- porca e parafuso.swift
+++ porca e parafuso.swift
@@ -11,7 +11,7 @@ struct Dimensoes
 
 extension Parafuso
 {
-    static func * (left: Parafuso, right: Porca)-> Bool
+    static func * (left: Parafuso, right: Porca)-> Bool//verifica se a bitola e o passo do parafuso são compativeis e retorna verdadeiro ou falso caso o parafuso não case com a porca
     {
         // faz tudo
         return ((( left._Dimensoes.bitola - right._Dimensoes.bitola ) == 0) && (( left._Dimensoes.passo - right._Dimensoes.passo ) == 0))
@@ -25,7 +25,7 @@ class Parafuso
         _Dimensoes = Dimensoes(bitola: a, passo: b)
     }
 }
-class Porca
+class Porca// 
 {
     var _Dimensoes: Dimensoes
     init(a: Float, b: Float)
@@ -34,7 +34,7 @@ class Porca
     }
 }
 
-let parafuso04CBF = Parafuso(a: 3.0, b: 2.0)
-let porca3876_34 = Porca(a: 3.0, b: 2.0)
-let validado = parafuso04CBF * porca3876_34
+let parafuso04CBF = Parafuso(a: 3.0, b: 2.0)//cria um objeto da classe parafuso 
+let porca3876_34 = Porca(a: 3.0, b: 2.0)//cria um objeto da classe proca
+let validado = parafuso04CBF * porca3876_34// usa o operando * para substiruir uma função 
 print(validado)
